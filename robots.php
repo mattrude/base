65eb488b0da2d6ab069fef976fcfb4f2182d372a
Matt Rude <github@mattrude.com>
2009-11-01 14:40:30 -0600

    add the rest of the robots functions

--- robots.php
+++ robots.php
@@ -1,4 +1,29 @@
 <?php
+
+// Adds robots.txt support
+$defaultrobotstxt = "# This is the default robots.txt file
+User-agent: *
+Allow: /
+";
+
+add_option("robots_txt", $defaultrobotstxt, "Contents of robots.txt", 'no');            // default value
+
+function robots_txt(){
+        $request = str_replace( get_bloginfo('url'), '', 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'] );
+
+        if ( (get_bloginfo('url').'/robots.txt' != 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']) && ('/robots.txt' != $_SERVER['REQUEST_URI']) && ('robots.txt' != $_SERVER['REQUEST_URI']) )
+                return;         // checking whether they're requesting robots.txt
+
+        $robotstxt_out = get_option('robots_txt');
+
+        if ( !$robotstxt_out)
+                return;
+
+        header('Content-type: text/plain');
+        print $robotstxt_out;
+        die;
+}
+
 function mdr_robots_controlpanel() {
   if ( $_POST['robots_txt'] ){
     update_option( 'robots_txt', $_POST['robots_txt'] );
