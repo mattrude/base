1c9c5811bd33eb88298ca1db7aeb168dfa8e23e6
Matt Rude <github@mattrude.com>
2009-11-01 15:13:59 -0600

    fix robots.php file

--- robots.php
+++ robots.php
@@ -3,26 +3,19 @@
 // Adds robots.txt support
 $defaultrobotstxt = "# This is the default robots.txt file
 User-agent: *
-Allow: /
-";
+Disallow:";
 
 add_option("robots_txt", $defaultrobotstxt, "Contents of robots.txt", 'no');            // default value
 
-function robots_txt(){
-        $request = str_replace( get_bloginfo('url'), '', 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'] );
-
-        if ( (get_bloginfo('url').'/robots.txt' != 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']) && ('/robots.txt' != $_SERVER['REQUEST_URI']) && ('robots.txt' != $_SERVER['REQUEST_URI']) )
-                return;         // checking whether they're requesting robots.txt
-
-        $robotstxt_out = get_option('robots_txt');
-
-        if ( !$robotstxt_out)
-                return;
-
-        header('Content-type: text/plain');
-        print $robotstxt_out;
-        die;
-}
+$request = str_replace( get_bloginfo('url'), '', 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'] );
+if ( (get_bloginfo('url').'/robots.txt' != 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']) && ('/robots.txt' != $_SERVER['REQUEST_URI']) && ('robots.txt' != $_SERVER['REQUEST_URI']) )
+  return;         // checking whether they're requesting robots.txt
+  $robotstxt_out = get_option('robots_txt');
+  if ( !$robotstxt_out)
+  return;
+    header('Content-type: text/plain');
+    print $robotstxt_out;
+die;
 
 function mdr_robots_controlpanel() {
   if ( $_POST['robots_txt'] ){
@@ -37,6 +30,7 @@ function mdr_robots_controlpanel() {
   }
 
   $robotstxt_out = get_option('robots_txt');
+  echo $robotstxt_out;
 
   print '
   <div class="wrap">
@@ -62,4 +56,5 @@ function mdr_robots_controlpanel() {
   </div>
   ';
 }
+
 ?>
