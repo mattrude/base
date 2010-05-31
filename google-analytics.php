8f47ef32fd195775618e4e38ce321268c6b11f2e
Matt Rude <m@mattrude.com>
2010-05-30 21:31:42 -0500

    Added Google Analytics Asynchronous Tracking Function   http://code.google.com/apis/analytics/docs/tracking/asyncTracking.html

--- google-analytics.php
+++ google-analytics.php
@@ -34,4 +34,42 @@ function google_analytics() {
     }
   } 
 }
+
+function async_google_analytics() {
+  if ( is_user_logged_in() ) {
+    ?>
+    <!--User is logged in, so this request will NOT be tracked by Google Asynchronous Analytics-->
+    <?php
+  } else {
+    global $Panel;
+    $GAEnabled = $Panel->Settings('GoogleAnalyticsEnabled');
+    $GAID = $Panel->Settings('GoogleAnalyticsID');
+    if ($GAEnabled == 'true') {
+      if ($GAID != NULL) { ?>
+      <!--Begin Google Asynchronous Analytics tracking script-->
+      <script type="text/javascript">
+        var _gaq = _gaq || [];
+        _gaq.push(['_setAccount', '<?php echo $GAID; ?>']);
+        _gaq.push(['_trackPageview']);
+
+        (function() {
+          var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+          ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+          (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(ga);
+        })();
+        }
+      </script>
+      <!--End Google Asynchronous Analytics tracking script-->
+      <?php } else {
+        ?>
+        <!--No user is logged in and Google Asynchronous Analytics is enabled, but there is not Google Analytics ID filled in-->
+        <?php
+      }
+    } else {
+      ?>
+      <!--No user is logged in and Google Asynchronous Analytics is disabled-->
+      <?php
+    }
+  }
+}
 ?>
